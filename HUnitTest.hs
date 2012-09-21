74d8954a94c2d8a92fb89de7d5e1c4982fa2d5a6
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2012-09-21 14:38:56 +0200

    report done

--- HUnitTest.hs
+++ HUnitTest.hs
@@ -5,7 +5,17 @@ import World
 
 
 
-test1Maze = fromList [((0,0),[North,South,West]),((0,1),[North,South,West]),((0,2),[South,West]),((0,3),[West,East]),((0,4),[North,West]),((1,0),[South]),((1,1),[North]),((1,2),[South,East]),((1,3),[North,West]),((1,4),[North,South,East]),((2,0),[North,South]),((2,1),[South,East]),((2,2),[West,East]),((2,3),[]),((2,4),[North,West,East]),((3,0),[North,South]),((3,1),[South,West]),((3,2),[West]),((3,3),[]),((3,4),[North,West,East]),((4,0),[North,South,East]),((4,1),[North,South,East]),((4,2),[North,South,East]),((4,3),[South,East]),((4,4),[North,West,East])]
+test1Maze = fromList [((0,0),[North,South,West]),((0,1),[North,South,West]),
+                      ((0,2),[South,West]),((0,3),[West,East]),
+                      ((0,4),[North,West]),((1,0),[South]),((1,1),[North]),
+                      ((1,2),[South,East]),((1,3),[North,West]),
+                      ((1,4),[North,South,East]),((2,0),[North,South]),
+                      ((2,1),[South,East]),((2,2),[West,East]),((2,3),[]),
+                      ((2,4),[North,West,East]),((3,0),[North,South]),
+                      ((3,1),[South,West]),((3,2),[West]),((3,3),[]),
+                      ((3,4),[North,West,East]),((4,0),[North,South,East]),
+                      ((4,1),[North,South,East]),((4,2),[North,South,East]),
+                      ((4,3),[South,East]),((4,4),[North,West,East])]
 
 test1 = TestCase $ assertBool "Go 3 north" $ (0,3) == (go 3 North (0,0))
 
@@ -44,13 +54,17 @@ test7Program = Program{statement=(Block [TurnRight,
 test7 = TestCase $ assertBool "runProg" $ (runProg test7Maze test7Program)
        == (Just ([(1,1),(1,0),(0,0)],East))
 
-test8Program1 = Program{statement=(Block [TurnRight, Forward, Forward, Forward])}
+test8Program1 = Program{statement=(Block [TurnRight, Forward, 
+                                          Forward, Forward])}
 
 test8Program2 = Program{statement=(Block [Forward, TurnRight, Forward])}
 
-test8Maze = fromList [((0,0),[South,West]),((0,1),[North,West,East]),((1,0),[South]),((1,1),[North,East,West])]
+test8Maze = fromList [((0,0),[South,West]),((0,1),[North,West,East]),
+                      ((1,0),[South]),((1,1),[North,East,West])]
 
-test8 = TestCase $ assertBool "runProg, walking into a border and a wall" $ (runProg test8Maze test8Program1) == Nothing && (runProg test8Maze test8Program2) == Nothing
+test8 = TestCase $ assertBool "runProg, walking into a border and a wall" $
+        (runProg test8Maze test8Program1) == Nothing &&
+        (runProg test8Maze test8Program2) == Nothing
 
 tests = TestList [test1, test2, test3, test4, test5, test6, test7, test8]
 
