bbba0b3ffa51e3f9379facf638a69a75e9e8ff93
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2012-09-21 08:43:42 +0200

    added testMaze2

--- World.hs
+++ World.hs
@@ -44,6 +44,7 @@ fromList lst = Maze (M.fromList lst) (maximum xCoords + 1) (maximum yCoords + 1)
 
 testMase = fromList [((0,0),[North,South,West]),((0,1),[North,South,West]),((0,2),[South,West]),((0,3),[West,East]),((0,4),[North,West]),((1,0),[South]),((1,1),[North]),((1,2),[South,East]),((1,3),[North,West]),((1,4),[North,South,East]),((2,0),[North,South]),((2,1),[South,East]),((2,2),[West,East]),((2,3),[]),((2,4),[North,West,East]),((3,0),[North,South]),((3,1),[South,West]),((3,2),[West]),((3,3),[]),((3,4),[North,West,East]),((4,0),[North,South,East]),((4,1),[North,South,East]),((4,2),[North,South,East]),((4,3),[South,East]),((4,4),[North,West,East])]
 testMaze = fromList [((0,0),[South,West]),((0,1),[North,West,East]),((1,0),[South,East]),((1,1),[North,East,West])]
+testMaze' = fromList [((0,0),[South,West]),((0,1),[North,West,East]),((1,0),[South]),((1,1),[North,East,West])]
 
 -- PART 2: A MEL interpreter
 
